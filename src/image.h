682ab779beda72495ae83da2c218ce04207c9fd0
Luke Orth <lorth001@gmail.com>
2022-06-02 10:21:11 -0500

    Adding maze.c and maze.h

---
+++ src/image.h
@@ -0,0 +1,29 @@
+#ifndef IMAGE_H
+#define IMAGE_H
+
+#include <png.h>
+#include <stdint.h>
+
+/* A coloured pixel. */
+typedef struct {
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+}
+pixel_t;
+
+/* A picture. */
+typedef struct {
+    pixel_t *pixels;
+    size_t width;
+    size_t height;
+}
+bitmap_t;
+
+pixel_t *pixel_at(bitmap_t *bitmap, int x, int y);
+
+int save_png_to_file(bitmap_t *bitmap, const char *path);
+
+int pix(int value, int max);
+
+#endif
--- src/maze.h
+++ src/maze.h
@@ -1,29 +1,16 @@
 #ifndef MAZE_H
 #define MAZE_H
 
-#include <png.h>
-#include <stdint.h>
+#include "image.h"
 
-/* A coloured pixel. */
 typedef struct {
-    uint8_t red;
-    uint8_t green;
-    uint8_t blue;
-}
-pixel_t;
+	pixel_t *pixels;
+	int width;
+	int height;
+	int line_weight;
+} cell_t;
 
-/* A picture. */
-typedef struct {
-    pixel_t *pixels;
-    size_t width;
-    size_t height;
-}
-bitmap_t;
+bitmap_t *init_maze(int width, int height);
 
-pixel_t *pixel_at(bitmap_t *bitmap, int x, int y);
-
-int save_png_to_file(bitmap_t *bitmap, const char *path);
-
-int pix(int value, int max);
 
 #endif
